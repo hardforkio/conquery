363b84d11bc8416205b3ad60147a06f0f33704d7
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-05-20 16:18:31 +0200

    WIP new FEAbilitiy structure

--- backend/src/main/java/com/bakdata/conquery/apiv1/MeProcessor.java
+++ backend/src/main/java/com/bakdata/conquery/apiv1/MeProcessor.java
@@ -1,13 +1,17 @@
 package com.bakdata.conquery.apiv1;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.bakdata.conquery.io.xodus.MasterMetaStorage;
 import com.bakdata.conquery.models.auth.AuthorizationHelper;
 import com.bakdata.conquery.models.auth.entities.Group;
 import com.bakdata.conquery.models.auth.entities.User;
+import com.bakdata.conquery.models.auth.permissions.Ability;
 import com.bakdata.conquery.models.auth.permissions.DatasetPermission;
+import com.bakdata.conquery.models.datasets.Dataset;
+import com.bakdata.conquery.models.identifiable.ids.specific.DatasetId;
 import com.bakdata.conquery.models.identifiable.ids.specific.GroupId;
 import com.bakdata.conquery.resources.admin.ui.model.FEPermission;
 import com.bakdata.conquery.resources.api.MeResource;
@@ -31,10 +35,15 @@ public class MeProcessor {
 	 * @return The information about the user
 	 */
 	public FEMeInformation getUserInformation(@NonNull User user){
+		for(Dataset dataset : storage.getNamespaces().getAllDatasets()) {
+			
+		}
+		
 		return FEMeInformation.builder()
 			.userName(user.getLabel())
 			.groups(FEGroup.from(AuthorizationHelper.getGroupsOf(user, storage)))
-			.permissions( FEPermission.from(AuthorizationHelper.getEffectiveUserPermissions(user.getId(), List.of(DatasetPermission.DOMAIN), storage).values()))
+			.abitities(FEAbilities.builder()
+				.onDataset(null).build()
 			.build();
 	}
 
@@ -64,9 +73,22 @@ public class MeProcessor {
 	@Data
 	@Builder
 	public static class FEMeInformation {
-		String userName;
-		List<FEPermission> permissions;
-		List<FEGroup> groups;
+		private String userName;
+		private FEAbilities abilities;
+		private List<FEGroup> groups;
+	}
+
+	@Data
+	@Builder
+	public static class FEAbilities {
+		private Map<DatasetId,FEDatasetAbilities> onDataset;
+	}
+
+	@Data
+	@Builder
+	public static class FEDatasetAbilities {
+		boolean canDownload;
+		boolean canUpload;
 	}
 
 }
