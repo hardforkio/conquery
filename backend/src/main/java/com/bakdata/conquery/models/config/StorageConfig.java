66e37f25406f6bcb1553473432e1a4de60bdc5e9
Max Thonagel <12283268+thoniTUB@users.noreply.github.com>
2020-07-01 10:16:56 +0200

    adds option to delete unreadble values from store

--- backend/src/main/java/com/bakdata/conquery/models/config/StorageConfig.java
+++ backend/src/main/java/com/bakdata/conquery/models/config/StorageConfig.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import com.bakdata.conquery.io.xodus.stores.SerializingStore;
 import io.dropwizard.util.Duration;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,6 +23,11 @@ public class StorageConfig {
 	private boolean useWeakDictionaryCaching = true;
 	@NotNull
 	private Duration weakCacheDuration = Duration.hours(48);
+	
+	/**
+	 * Flag for the {@link SerializingStore} whether to delete values from the underlying store, that cannot be mapped to an object anymore.
+	 */
+	private boolean removeUnreadablesFromStore = false;
 	
 	/**
 	 * When set, all values that could not be deserialized from the persistent store, are dump into individual files.
